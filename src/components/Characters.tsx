557cdf054aa794acd64711bb7c6c0e0a68d7fe39
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-02-07 15:12:53 +0000

    formatting

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -3,54 +3,53 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Characters(): JSX.Element {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState<any[]>([]);
-    const [errorMessage, setErrorMessage] = useState<null | string>(null)
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<any[]>([]);
+  const [errorMessage, setErrorMessage] = useState<null | string>(null);
 
-    async function fetchData() {
-        try {
-            const response = await axios.get(
-                "https://hp-api.onrender.com/api/characters"
-            );
-            setData(response.data);
-            setLoading(false);
-        }
-        catch (error: any) {
-            setErrorMessage(error.message)
-            setLoading(false)
-        }
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        "https://hp-api.onrender.com/api/characters"
+      );
+      setData(response.data);
+      setLoading(false);
+    } catch (error: any) {
+      setErrorMessage(error.message);
+      setLoading(false);
     }
+  }
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    if (loading) {
-        return <h1>Getting Data...</h1>;
-    }
+  if (loading) {
+    return <h1>Getting Data...</h1>;
+  }
 
-    if (errorMessage) {
-        return <h1>{errorMessage}</h1>
-    }
+  if (errorMessage) {
+    return <h1>{errorMessage}</h1>;
+  }
 
-    return (
-        <>
-            <h1>Normal Page</h1>
-            <li>
-                <Link to={"/"}>Home Page</Link>
-            </li>
-            <li>
-                <Link to={"/react-query"}>React Query Page</Link>
-            </li>
-            <hr />
-            {data.map((character) => {
-                return (
-                    <div className="character" key={character.id}>
-                        <p>{character.name}</p>
-                        <img src={character.image} alt={character.name} />
-                    </div>
-                );
-            })}
-        </>
-    );
+  return (
+    <>
+      <h1>Normal Page</h1>
+      <li>
+        <Link to={"/"}>Home Page</Link>
+      </li>
+      <li>
+        <Link to={"/react-query"}>React Query Page</Link>
+      </li>
+      <hr />
+      {data.map((character) => {
+        return (
+          <div className="character" key={character.id}>
+            <p>{character.name}</p>
+            <img src={character.image} alt={character.name} />
+          </div>
+        );
+      })}
+    </>
+  );
 }
